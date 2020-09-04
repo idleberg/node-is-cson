337d11e03a5629aae7230a97c00f8d3a5a37535c
idleberg <git@idleberg.com>
2020-09-04 14:10:11 +0200

    switch to Rollup/ESLint

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+import typescript from '@rollup/plugin-typescript';
+
+const defaults = {
+  output: {
+    dir: 'dist',
+    format: 'cjs'
+  },
+  plugins: [
+    commonjs(),
+    json(),
+    nodePolyfills(),
+    nodeResolve(),
+    typescript({
+      "lib": [
+          "dom",
+          "esnext"
+      ],
+      "module": "commonjs",
+      "moduleResolution": "node"
+    })
+  ]
+};
+
+export default [
+  {
+    ...defaults,
+    input: 'src/index.ts',
+  }
+];
