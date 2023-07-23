53b4ac876d1b360458dd483409c017304bba33e7
Jan T. Sott <git@idleberg.com>
2023-07-23 23:40:54 +0200

    use lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,25 @@
 /* eslint-disable */
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: {},
-  ignorePatterns: [
-    'test/**/*'
-  ]
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'@typescript-eslint',
+	],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:json/recommended'
+	],
+	rules: {},
+	ignorePatterns: [
+		'test/**/*'
+	],
+	"overrides": [
+		{
+			"files": ["tsconfig.json"],
+			"rules": {
+				"json/*": ["error", "allowComments"]
+			}
+		}
+	]
 };
